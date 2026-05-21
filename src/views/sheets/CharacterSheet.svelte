33ffbd3201ad616de1e191e39f8f1c981088ea2f
BaloonKiller <afrackiewicz250@gmail.com>
2026-05-22 01:22:13 +0200

    fix

--- src/views/sheets/CharacterSheet.svelte
+++ src/views/sheets/CharacterSheet.svelte
@@ -83,26 +83,16 @@
    </header>
    <section id="tab-sections">
       <nav class="tabs bg-gradient-to-r from-white to-table-name border-y my-4 font-bold">
-         <a href="#main" class="item px-2.5 py-1 w-full" data-tab="main" on:click|preventDefault>
-            {localize("EDRPG.sheet.tabs.main")}
-         </a>
-         <a href="#skills" class="item px-2.5 py-1 w-full" data-tab="skills" on:click|preventDefault>
-            {localize("EDRPG.sheet.tabs.skills")}
-         </a>
-         <a
-            href="#backgrounds-karma"
-            class="item px-2.5 py-1 w-full"
-            data-tab="backgrounds-karma"
-            on:click|preventDefault
-         >
-            {localize("EDRPG.sheet.tabs.bgKarma")}
-         </a>
-         <a href="#combat" class="item px-2.5 py-1 w-full" data-tab="combat" on:click|preventDefault>
-            {localize("EDRPG.sheet.tabs.combat")}
-         </a>
-         <a href="#equipment" class="item px-2.5 py-1 w-full" data-tab="equipment" on:click|preventDefault>
-            {localize("EDRPG.sheet.tabs.equipment")}
-         </a>
+         <!-- svelte-ignore a11y-missing-attribute -->
+         <a class="item px-2.5 py-1 w-full" data-tab="main">{localize("EDRPG.sheet.tabs.main")}</a>
+         <!-- svelte-ignore a11y-missing-attribute -->
+         <a class="item px-2.5 py-1 w-full" data-tab="skills">{localize("EDRPG.sheet.tabs.skills")}</a>
+         <!-- svelte-ignore a11y-missing-attribute -->
+         <a class="item px-2.5 py-1 w-full" data-tab="backgrounds-karma">{localize("EDRPG.sheet.tabs.bgKarma")}</a>
+         <!-- svelte-ignore a11y-missing-attribute -->
+         <a class="item px-2.5 py-1 w-full" data-tab="combat">{localize("EDRPG.sheet.tabs.combat")}</a>
+         <!-- svelte-ignore a11y-missing-attribute -->
+         <a class="item px-2.5 py-1 w-full" data-tab="equipment">{localize("EDRPG.sheet.tabs.equipment")}</a>
       </nav>
       <section class="tab-content">
          <div class="tab" data-tab="main"></div>
